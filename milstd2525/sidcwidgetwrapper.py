9212f22552f9fd07edb2506194aad917266d12aa
Alexander Bruy <alexander.bruy@gmail.com>
2016-03-11 11:04:32 +0200

    add license headers to sources

--- milstd2525/sidcwidgetwrapper.py
+++ milstd2525/sidcwidgetwrapper.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+"""
+***************************************************************************
+    sidcwidgetwrapper.py
+    ---------------------
+    Date                 : December 2015
+    Copyright            : (C) 2015-2016 Boundless, http://boundlessgeo.com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Victor Olaya'
+__date__ = 'December 2015'
+__copyright__ = '(C) 2015-2016 Boundless, http://boundlessgeo.com'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
 import os
 
 from PyQt4 import uic
