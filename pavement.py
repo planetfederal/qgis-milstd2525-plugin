a3ce42989d85ff4ff25e0e10efbf4848b3af5a9c
volaya <volayaf@gmail.com>
2018-10-25 10:51:32 +0200

    use pip instead of easy_install

--- pavement.py
+++ pavement.py
@@ -45,7 +45,7 @@ def setup():
     runtime, test = read_requirements()
     os.environ['PYTHONPATH']=ext_libs.abspath()
     for req in runtime + test:
-        sh('easy_install -a -d %(ext_libs)s %(dep)s' % {
+        sh('pip install -U -t %(ext_libs)s %(dep)s' % {
             'ext_libs' : ext_libs.abspath(),
             'dep' : req
         })
